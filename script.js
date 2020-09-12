6ed42c017e22be3010fbb819ff0b371416e90c05
dipuchaudhary <dipuchaudhary8@gmail.com>
2020-09-12 11:34:50 +0545

    project added

---
+++ script.js
@@ -0,0 +1,61 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+populateUI();
+
+// save movie data and movie price
+function updateMovieData(movieIndex,moviePrice) {
+    localStorage.setItem('selectedMovieIndex',movieIndex);
+    localStorage.setItem('selectedMoviePrice',moviePrice);
+}
+// movie select event
+movieSelect.addEventListener('change', function(e){
+    ticketPrice = e.target.value;
+    updateMovieData(e.target.selectedIndex,e.target.value);
+    updateSelectedCount();
+})
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatIndex = [...selectedSeats].map(seat =>[...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats',JSON.stringify(seatIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Get data from localstorage and populate UI 
+function populateUI() {
+const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+if(selectedSeats!= null && selectedSeats.length > 0) {
+    seats.forEach((seat,index) => {
+        if(selectedSeats.indexOf(index) > -1) {
+            seat.classList.add('selected');
+        } 
+    });
+}
+
+const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+if(selectedMovieIndex != null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+}
+}
+
+// seat select event
+container.addEventListener('click',function(e) {
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+})
+
+// initial count and total select
+updateSelectedCount();
